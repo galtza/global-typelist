f6380fc27f8cac80dc7b9fa1590002238a92da03
Raul Ramos <igaltza@gmail.com>
2018-03-28 00:27:19 +0100

    Simplified the code to show only things mentioned in the article

--- main.cpp
+++ main.cpp
@@ -1,41 +1,20 @@
-#include <iostream>
-#include <iomanip>
-#include <array>
-#include <utility>
-#include <stdio.h>
+#include <type_traits>
 #include "tmp.h"
 
-using namespace tmp;
-
-template<typename TL>
-struct hierarchy_iterator {
-    static_assert(is_typelist<TL>::value, "Not a typelist");
-    inline static void exec(void* _p) {
-        using target_t = typename pop_front<TL>::type;
-        if (auto ptr = static_cast<target_t*>(_p)) {
-            printf("base = %s\n", typeid(typename at<TL, 0>::type).name());
-            hierarchy_iterator<target_t>::exec(_p);
-        }
-    }
-};
-
-template<>
-struct hierarchy_iterator<typelist<>> {
-    inline static void exec(void*) {
-    }
-};
+// Macros used to create a NOISE macro that forces __COUNTER__ to increase
+#define _WRAP_PASTE(x,y) x##y
+#define _MERGE(x,y) _WRAP_PASTE(x,y)
+#define NOISE auto _MERGE(unused, __COUNTER__) = __COUNTER__
 
 /*
-=================================================================
+   Class hierarchies
                                     F
                                    / \
      A                            H   \
     / \                          / \   \
    B   C                        I   J   G
   /   / \                        \ /   / \
- T   D   E                        K   L   Z
-================================================================= */
-
+ T   D   E                        K   L   Z         */
 class A { };                   class F { };
 class B : public A { };        class G : public F { };
 class C : public A { };        class L : public G { };
@@ -45,73 +24,36 @@ class E : public C { };        class I : public H { };
                                class J : public H { };
                                class K : public I, public J { };
 
-// ==============================================================
-#define _T(x) #x
-#define STR(x) _T(x)
-#define CONCAT(x, y) (x ## y)
-#define MSG "ERROR at line " STR(__LINE__)
-// ==============================================================
-#define _WRAP_PASTE(x,y) x##y
-#define _MERGE(x,y) _WRAP_PASTE(x,y)
-#define _UNIQUE_UNUSED _MERGE(unused, __COUNTER__)
-// ==============================================================
 
-#define SERIALIZE(_instance, _tl) hierarchy_iterator<find_ancestors<_tl, decltype(_instance)>::type>::exec(&_instance)
-#define NOISE auto _UNIQUE_UNUSED = __COUNTER__
-
+// Declare the meta-variable
 DECLARE_TL(registry);
 
-ADD_TL(registry, I);
-
-NOISE;
+// Add new types and stress test the solution with calls to __COUNTER__
+ADD_TL(registry, I); NOISE;
 ADD_TL(registry, C);
-ADD_TL(registry, Z);
-NOISE;
-NOISE;
-ADD_TL(registry, G);
-NOISE;
-NOISE;
+ADD_TL(registry, Z); NOISE; NOISE;
+ADD_TL(registry, G); NOISE; NOISE; NOISE;
 ADD_TL(registry, D);
 ADD_TL(registry, F);
-ADD_TL(registry, L);
-NOISE;
-NOISE;
-ADD_TL(registry, C);
-NOISE;
-ADD_TL(registry, I);
-NOISE;
-ADD_TL(registry, A);
-NOISE;
+ADD_TL(registry, L); NOISE;
+ADD_TL(registry, C); NOISE; NOISE; NOISE; NOISE; 
+ADD_TL(registry, I); NOISE;
+ADD_TL(registry, A); NOISE;
 ADD_TL(registry, T);
 ADD_TL(registry, B);
 ADD_TL(registry, J);
-ADD_TL(registry, K);
-NOISE;
-NOISE;
-NOISE;
-NOISE;
-NOISE;
+ADD_TL(registry, K); NOISE; NOISE; NOISE; NOISE; NOISE;
 ADD_TL(registry, H);
-ADD_TL(registry, E);
 ADD_TL(registry, E);
 
 int main()
 {
-    using namespace std;
-    using namespace tmp;
-
-    static_assert(is_same<READ_TL(registry),                          typelist<I, C, Z, G, D, F, L, C, I, A, T, B, J, K, H, E, E>>::value, MSG);
-    static_assert(is_same<find_ancestors<READ_TL(registry), D>::type, typelist<A, C, D>>::value,                                           MSG);
-    static_assert(is_same<find_ancestors<READ_TL(registry), K>::type, typelist<F, H, J, I, K>>::value,                                     MSG);
-
-    D d_instance;
-    SERIALIZE(d_instance, READ_TL(registry));
-
-    printf("\n\n");
-
-    K k_instance;
-    SERIALIZE(k_instance, READ_TL(registry));
+    // Check that the constructed type list is what we expected
+    using expected_registry = tmp::typelist<I, C, Z, G, D, F, L, C, I, A, T, B, J, K, H, E>;
+    static_assert(
+        std::is_same<READ_TL(registry), expected_registry>::value,
+        "Error: unexpected registry type list"
+    );
 
     return 0;
-}
-
+} 
--- tmp.h
+++ tmp.h
@@ -10,123 +10,13 @@ namespace tmp {
         static constexpr auto size = sizeof...(TS);
     };
 
-    template<typename T>
-    struct is_typelist : std::false_type { };
-
-    template<typename...TS>
-    struct is_typelist<typelist<TS...>> : std::true_type { };
-
     // basic operations
-    template<typename T, typename TL> struct push_back;
-    template<typename T, typename TL> struct push_front;
-    template<typename TL>             struct pop_front;
-    template<typename TL, size_t I>   struct at;
+    template<typename T, typename TL> 
+    struct push_back;
 
     template<typename T, typename...TS>
     struct push_back<T, typelist<TS...>> {
         using type = typelist<TS..., T>;
-    };
-
-    template<typename T, typename...TS>
-    struct push_front<T, typelist<TS...>> {
-        using type = typelist<T, TS...>;
-    };
-
-    template<typename T, typename...TS>
-    struct pop_front<typelist<T, TS...>> {
-        using type = typelist<TS...>;
-    };
-
-    template <typename T, typename...TS>
-    struct at<typelist<T, TS...>, 0> {
-        using type = T;
-    };
-
-    template <typename T, typename...TS, size_t I>
-    struct at<typelist<T, TS...>, I> {
-        static_assert(I < (1 + sizeof...(TS)), "Out of bounds access");
-        using type = typename at<typelist<TS...>, I - 1>::type;
-    };
-
-    // 'filter'
-    template<typename TL, template<typename>class PRED>
-    struct filter;
-
-    template <template<typename>class PRED>
-    struct filter<typelist<>, PRED> {
-        using type = typelist<>;
-    };
-
-    template <typename T, typename...TS, template<typename>class PRED>
-    struct filter<typelist<T, TS...>, PRED> {
-        using remaining = typename filter<typelist<TS...>, PRED>::type;
-        using type = typename std::conditional<
-            PRED<T>::value,
-            typename push_front<T, remaining>::type,
-            remaining
-        >::type;
-    };
-
-    // 'max' given a template binary predicate
-    template<typename TL, template<typename,typename>class PRED>
-    struct max;
-
-    template <typename T, template<typename,typename>class PRED>
-    struct max<typelist<T>, PRED> {
-        using type = T;
-    };
-
-    template <typename ...TS, template<typename,typename>class PRED>
-    struct max<typelist<TS...>, PRED> {
-        using first = typename at<typelist<TS...>, 0>::type;
-        using remaining_max = typename max<typename pop_front<typelist<TS...>>::type, PRED>::type;
-        using type  = typename std::conditional<
-            PRED<first, remaining_max>::value,
-            first, remaining_max
-        >::type;
-    };
-
-    // 'find_ancestors'
-    namespace impl {
-
-        template<typename SRCLIST, typename DESTLIST>
-        struct find_ancestors {
-
-            template<typename B>
-            using negation = typename std::integral_constant<bool, !bool(B::value)>::type;
-
-            template<typename T, typename U>
-            using cmp = typename std::is_base_of<T, U>::type;
-            using most_ancient = typename max<SRCLIST, cmp>::type;
-
-            template<typename T>
-            using not_most_ancient = typename negation<std::is_same<most_ancient, T>>::type;
-
-            using type = typename find_ancestors<
-                typename filter<SRCLIST, not_most_ancient>::type,
-                typename push_back<most_ancient, DESTLIST>::type
-            >::type;
-
-        };
-
-        template<typename DESTLIST>
-        struct find_ancestors<typelist<>, DESTLIST> {
-            using type = DESTLIST;
-        };
-
-    }
-
-    template<typename TL, typename T>
-    struct find_ancestors {
-        static_assert(is_typelist<TL>::value, "The first parameter is not a typelist");
-
-        template<typename U>
-        using base_of_T = typename std::is_base_of<U, T>::type;
-        using src_list = typename filter<TL, base_of_T>::type;
-        using type = typename impl::find_ancestors<
-            src_list,
-            typelist<>
-        >::type;
     };
 
     // Check if a type is defined or not
@@ -138,46 +28,44 @@ namespace tmp {
 
 #define DECLARE_TL(_name) _DECLARE_TL(_name, __COUNTER__)
 #define _DECLARE_TL(_name, _start)\
-    namespace tmp {\
-        /* Create the meta-variable */\
-        template<size_t IDX> \
-        struct _name##_history;\
-        \
-        /* Initialize it empty */\
-        template<> struct _name##_history<_start> {\
-            using type = tmp::typelist<>;\
-        };\
-        \
-        /* Select the current typelist at index "I" */\
-        template <size_t I>\
-        using _name##_is_defined = decltype(is_class_complete(std::declval<_name##_history<I>*>()));\
-        \
-        template<size_t I, bool = std::is_same<std::true_type, _name##_is_defined<I>>::value>\
-        struct _name##_read;\
-        \
-        template<size_t I>\
-        struct _name##_read<I, true> {\
-            using type = typename _name##_history<I>::type;\
-        };\
-        \
-        template<size_t I>\
-        struct _name##_read<I, false> {\
-            using type = typename std::conditional< \
-                (I > _start),                     /* Are there more specializations to check? */\
-                typename _name##_read<I-1>::type, /* yes */\
-                tmp::typelist<>                   /* no => failed => empty typelist */\
-            >::type;\
-        };\
+    /* Declare the type list meta-variable and initialize with an empty list */\
+    template<size_t IDX>\
+    struct _name##_history;\
+    \
+    template<> struct _name##_history<_start> {\
+        using type = tmp::typelist<>;\
+    };\
+    \
+    /* Check if the entry at "IDX" exists */\
+    template <size_t IDX>\
+    using _name##_is_defined = decltype(\
+        tmp::is_class_complete(std::declval<_name##_history<IDX>*>())\
+    );\
+    \
+    /* Read from an index IDX */\
+    template<size_t IDX, bool = std::is_same<std::true_type, _name##_is_defined<IDX>>::value>\
+    struct _name##_read;\
+    \
+    template<size_t IDX>\
+    struct _name##_read<IDX, true> {\
+        using type = typename _name##_history<IDX>::type;\
+    };\
+    \
+    template<size_t IDX>\
+    struct _name##_read<IDX, false> {\
+        using type = typename std::conditional< \
+            (IDX > _start),                     /* Should we stop searching? */\
+            typename _name##_read<IDX-1>::type, /* No */\
+            tmp::typelist<>                     /* Yes => failed => empty typelist */\
+        >::type;\
     }
 
 #define READ_TL(_name) typename _name##_read<__COUNTER__ - 1>::type
 #define _ADD_TL(_name, _class, _idx)\
     /* Define the current typelist at index _idx */\
-    namespace tmp {\
-        template<>\
-        struct _name##_history<_idx> {\
-            using previous = typename _name##_read<_idx - 1>::type;\
-            using type = typename push_back<_class, previous>::type;\
-        };\
+    template<>\
+    struct _name##_history<_idx> {\
+        using previous = typename _name##_read<_idx - 1>::type;\
+        using type = typename tmp::push_back<_class, previous>::type;\
     }
 #define ADD_TL(_name, _class) _ADD_TL(_name, _class, __COUNTER__)
